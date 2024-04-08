4bf461bb5c910a56315dde11d7fd43a9bd419cb2
Rahardiann <yt005881@gmail.com>
2024-04-09 02:20:16 +0700

    update

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Mengambil lebar layar
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: ListView(
         children: [
@@ -32,11 +36,11 @@ class HomePage extends StatelessWidget {
                     ),
                   ],
                 ),
-                SizedBox(height: 80),
+                SizedBox(height: 50),
                 Padding(
                   padding: EdgeInsets.only(left: 3, bottom: 15),
                   child: Text(
-                    "Sugeng rawuh dan sugeng hancok",
+                    "Join and enjoy the benefits of being our member",
                     style: TextStyle(fontSize: 11),
                   ),
                 ),
@@ -52,13 +56,148 @@ class HomePage extends StatelessWidget {
                     ),
                     child: Text(
                       'Login or Register',
-                      style: TextStyle(color: Color(0xFF037F74)), // set text color to the specified color
+                      style: TextStyle(color: Color(0xFF037F74)),
                     ),
                   ),
+                ),
+                SizedBox(height: 20), // Tambahkan spasi sebelum carousel
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 200.0,
+              enlargeCenterPage: true,
+              autoPlay: true,
+              aspectRatio: 16 / 9,
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enableInfiniteScroll: true,
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              viewportFraction: 0.8,
+            ),
+            items: [
+              // Contoh gambar slide
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: Image.asset(
+                  'assets/slide1.jpeg',
+                  fit: BoxFit.cover,
+                  width: screenWidth, // Set lebar gambar menjadi lebar layar
+                ),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: Image.asset(
+                  'assets/slide2.jpeg',
+                  fit: BoxFit.cover,
+                  width: screenWidth, // Set lebar gambar menjadi lebar layar
+                ),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: Image.asset(
+                  'assets/slide3.jpeg',
+                  fit: BoxFit.cover,
+                  width: screenWidth, // Set lebar gambar menjadi lebar layar
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 40),
+          // Judul dan subjudul
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Promo",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFFB6366D),
+                      ),
+                    ),
+                    SizedBox(
+                        height: 5), // Tambahkan spasi antara judul dan subjudul
+                    Text(
+                      "Find attractive offers in our promotions",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                IconButton(
+                  icon: Icon(Icons.keyboard_arrow_right),
+                  onPressed: () {
+                    // Action when arrow button is pressed
+                  },
                 ),
               ],
             ),
           ),
+          SizedBox(height: 40),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.asset(
+                          'assets/slide2.jpeg',
+                          width: screenWidth * 0.4,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'New patient discount',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.asset(
+                          'assets/slide3.jpeg',
+                          width: screenWidth * 0.4,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Routine check-up discounts',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
