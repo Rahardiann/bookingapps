01bcd2a6b5dec71ed6dfc6dc1a305dd7c05a7d8a
Rahardiann <yt005881@gmail.com>
2024-04-08 18:25:09 +0700

    update appbar

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,27 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Home Page',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-    );
-  }
-}
-
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xffD7F0EE),
         leading: IconButton(
           icon: Icon(Icons.notifications),
           onPressed: () {
@@ -47,35 +31,46 @@ class HomePage extends StatelessWidget {
                 bottomRight: Radius.circular(20.0),
               ),
             ),
-            height: 100.0,
-            width: double.infinity,
-            margin: EdgeInsets.only(top: AppBar().preferredSize.height),
+            height: 150.0,
           ),
           Positioned.fill(
-            bottom: 8.0, // Margin bawah 8.0 pada Container
+            bottom: 20.0, // Margin bawah 8.0 pada Container
             child: Align(
               alignment: Alignment.bottomCenter,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(19),
-                  ),
-                ),
-                // Lakukan validasi login atau tindakan lain di sini
-                // Misalnya, print pesan "Login berhasil" untuk menunjukkan tombol berfungsi
-                onPressed: () {
-                  // Tambahkan logika untuk tombol login di sini
-                },
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 12.0),
-                  child: Text(
-                    "Login or Regrister",
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "Join and enjoy the benefits of being our member",
                     style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xFF037F74)
+                      fontSize: 11.0,
+                      color: Colors.black,
                     ),
                   ),
-                ),
+                  SizedBox(height: 10.0), // Spasi antara teks dan tombol
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(19),
+                      ),
+                    ),
+                    // Lakukan validasi login atau tindakan lain di sini
+                    // Misalnya, print pesan "Login berhasil" untuk menunjukkan tombol berfungsi
+                    onPressed: () {
+                      // Tambahkan logika untuk tombol login di sini
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 100.0, vertical: 12.0),
+                      child: Text(
+                        "Login or Register",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF037F74)
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
