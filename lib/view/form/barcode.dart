364aa1f55c733397ab366a9bddc5f458efa87e89
Rahardiann <yt005881@gmail.com>
2024-05-18 19:45:02 +0700

    history

--- lib/view/form/barcode.dart
+++ lib/view/form/barcode.dart
@@ -1,8 +1,6 @@
-import 'package:booking/view/login.dart';
 import 'package:flutter/material.dart';
 import 'package:booking/view/booking.dart';
 import 'package:booking/view/home.dart';
-import 'package:booking/view/form/editprofile.dart';
 
 class Barcode extends StatefulWidget {
   @override
@@ -10,38 +8,33 @@ class Barcode extends StatefulWidget {
 }
 
 class _ProfilesState extends State<Barcode> {
-  int _selectedIndex = 2; // Set indeks sesuai dengan "Profile"
+  int _selectedIndex = 2;
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
 
-    // Tambahkan logika navigasi di sini sesuai dengan masing-masing index
     switch (index) {
       case 0:
-        // Navigasi ke halaman Home
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => Home()),
         );
         break;
       case 1:
-        // Navigasi ke halaman Booking
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => Booking()),
         );
         break;
       default:
-        // Tidak perlu navigasi untuk halaman Profile
         break;
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    // Mengambil lebar layar
     double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
@@ -55,19 +48,18 @@ class _ProfilesState extends State<Barcode> {
                 Navigator.pop(context);
               },
               style: TextButton.styleFrom(
-                primary: Colors.black, // Mengatur warna teks tombol
+                primary: Colors.black,
               ),
               child: Row(
                 children: [
-                  Icon(Icons.arrow_back_ios,
-                      size: 20, color: Colors.black), // Mengatur warna ikon
+                  Icon(Icons.arrow_back_ios, size: 20, color: Colors.black),
                   SizedBox(width: 5),
                   Text(
                     'Back',
                     style: TextStyle(
                       fontSize: 14,
                       fontWeight: FontWeight.bold,
-                      color: Colors.black, // Mengatur warna teks
+                      color: Colors.black,
                     ),
                   ),
                 ],
@@ -78,7 +70,7 @@ class _ProfilesState extends State<Barcode> {
               style: TextStyle(
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
-                color: Colors.black, // Mengatur warna teks
+                color: Colors.black,
               ),
             ),
             SizedBox(width: 90),
@@ -90,57 +82,42 @@ class _ProfilesState extends State<Barcode> {
           Container(
             decoration: BoxDecoration(
               color: Color(0xFFD7F0EE),
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(15), // Sudut kiri atas tetap bulat
-                bottomLeft: Radius.circular(15), // Sudut kiri bawah tetap bulat
-                topRight: Radius.circular(15), // Sudut kanan atas tetap bulat
-                bottomRight:
-                    Radius.circular(15), // Sudut kanan bawah tetap bulat
-              ),
+              borderRadius: BorderRadius.all(Radius.circular(15)),
             ),
             padding: EdgeInsets.symmetric(horizontal: 1, vertical: 20),
             margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
             height: 320,
             child: Stack(
               children: [
-                // Garis horizontal di bagian atas kontainer
-                
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    // Stack untuk menempatkan gambar di atas ikon QR
                     Stack(
                       children: [
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
-                            Positioned(
-                              top: 0,
-                              left: 0,
-                              child: Image.asset(
-                                'assets/splash.png',
-                                width: 150,
-                                height: 80,
-                              ),
+                            Image.asset(
+                              'assets/splash.png',
+                              width: 150,
+                              height: 80,
+                              errorBuilder: (context, error, stackTrace) {
+                                return Text('Image not found',
+                                    style: TextStyle(color: Colors.red));
+                              },
                             ),
                             Icon(Icons.qr_code_2, size: 130),
-                            // Gambar di atas ikon QR
                           ],
                         ),
                       ],
                     ),
-                    SizedBox(width: 10), // Spasi antara gambar dan teks
-                    // Stack untuk menempatkan teks
-
+                    SizedBox(width: 10),
                     Container(
-                      width: 2, // Lebar garis
-                      color: Color(0xFF037F74), // Warna garis
-                      height: 210, // Sesuaikan tinggi dengan ikon QR
-                      margin: EdgeInsets.symmetric(
-                        vertical: 5,
-                      ), // Sesuaikan jarak dari atas dan bawah
+                      width: 2,
+                      color: Color(0xFF037F74),
+                      height: 210,
+                      margin: EdgeInsets.symmetric(vertical: 5),
                     ),
-
                     SizedBox(width: 10),
                     Expanded(
                       child: Stack(
@@ -151,15 +128,17 @@ class _ProfilesState extends State<Barcode> {
                               Text(
                                 'Jacob More',
                                 style: TextStyle(
-                                    fontSize: 24, fontWeight: FontWeight.bold),
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                              SizedBox(
-                                  height:
-                                      5), // Spasi antara teks utama dan subjudul
+                              SizedBox(height: 5),
                               Text(
                                 'Medical record | 001',
                                 style: TextStyle(
-                                    fontSize: 12, fontWeight: FontWeight.w600),
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w600,
+                                ),
                               ),
                               SizedBox(height: 50),
                               Row(
@@ -170,9 +149,7 @@ class _ProfilesState extends State<Barcode> {
                                       shape: BoxShape.circle,
                                       color: Color(0xFF037F74),
                                     ),
-                                    padding: EdgeInsets.all(
-                                      4,
-                                    ), // Atur ukuran padding sesuai kebutuhan
+                                    padding: EdgeInsets.all(4),
                                     child: Icon(Icons.location_on,
                                         size: 12, color: Colors.white),
                                   ),
@@ -185,9 +162,7 @@ class _ProfilesState extends State<Barcode> {
                                   ),
                                 ],
                               ),
-                              SizedBox(
-                                  height:
-                                      10), // Spasi antara alamat dan nomor telepon
+                              SizedBox(height: 10),
                               Row(
                                 children: [
                                   Container(
@@ -195,21 +170,17 @@ class _ProfilesState extends State<Barcode> {
                                       shape: BoxShape.circle,
                                       color: Color(0xFF037F74),
                                     ),
-                                    padding: EdgeInsets.all(
-                                      4,
-                                    ), // Atur ukuran padding sesuai kebutuhan
+                                    padding: EdgeInsets.all(4),
                                     child: Icon(Icons.phone,
                                         size: 12, color: Colors.white),
                                   ),
-                                  SizedBox(width: 5), // Icon telepon
-                                  // Spasi antara icon dan teks nomor telepon
+                                  SizedBox(width: 5),
                                   Text(
-                                    '+6281234567822', // Ganti dengan nomor telepon Anda
+                                    '+6281234567822',
                                     style: TextStyle(fontSize: 12),
                                   ),
                                 ],
                               ),
-                              
                             ],
                           ),
                         ],
@@ -217,22 +188,17 @@ class _ProfilesState extends State<Barcode> {
                     ),
                   ],
                 ),
-                
                 Positioned(
                   top: 240,
                   left: 100,
                   right: 0,
                   child: Container(
                     height: 20,
-                    width: double.infinity, // Mengisi lebar kontainer
+                    width: double.infinity,
                     color: Color(0xFF037F74),
-                    // Warna garis
                   ),
                 ),
-
-
-                // Garis horizontal di bagian bawah kontainer
-              Positioned(
+                Positioned(
                   top: 230,
                   left: 0,
                   right: 200,
@@ -242,25 +208,21 @@ class _ProfilesState extends State<Barcode> {
                     decoration: BoxDecoration(
                       color: Color.fromARGB(255, 219, 92, 147),
                       borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(20), // Sudut kanan atas
-                        bottomRight: Radius.circular(20), // Sudut kanan bawah
+                        topRight: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
                       ),
                       border: Border(
                         right: BorderSide(
-                          color: Colors.white, // Warna border putih
-                          width: 2, // Lebar border
+                          color: Colors.white,
+                          width: 2,
                         ),
                       ),
                     ),
                   ),
                 ),
-
-                 
               ],
             ),
-            
-          )
-
+          ),
         ],
       ),
     );
